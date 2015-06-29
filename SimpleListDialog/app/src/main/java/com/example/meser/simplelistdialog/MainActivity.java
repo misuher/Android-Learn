6cf33b78ffc58085239f4f6dd309e77d85f3e34c
misuher <cm.castillo87@gmail.com>
2015-06-30 00:40:42 +0200

    list dialog

--- SimpleListDialog/app/src/main/java/com/example/meser/simplelistdialog/MainActivity.java
+++ SimpleListDialog/app/src/main/java/com/example/meser/simplelistdialog/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.meser.simplelistdialog;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private String[] list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        list = getResources().getStringArray(R.array.lista);
+    }
+
+    public void show_dialoglist(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_title);
+        builder.setItems(R.array.lista, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, String.format("you choose %s", list[which]), Toast.LENGTH_SHORT).show();
+            }
+        });
+        Dialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
