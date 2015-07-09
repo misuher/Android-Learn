68fc26c31373acf04f14349a86be4803bfcab58d
misuher <cm.castillo87@gmail.com>
2015-07-09 23:32:24 +0200

    added list view, doest detect click events yet

--- SimpleListClickableItems/app/src/main/java/com/example/meser/simplelistclickableitems/MainActivity.java
+++ SimpleListClickableItems/app/src/main/java/com/example/meser/simplelistclickableitems/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String[] paises;
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        paises = getResources().getStringArray(R.array.paises);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                paises
+        );
+        lista = (ListView)findViewById(R.id.lista);
+        lista.setAdapter(adapter);
     }
 
     @Override
