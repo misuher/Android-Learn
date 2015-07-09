78f1d7d3290d30c1ae6adf331c9530c86fd067c2
misuher <cm.castillo87@gmail.com>
2015-07-09 23:47:23 +0200

    click listener added

--- SimpleListClickableItems/app/src/main/java/com/example/meser/simplelistclickableitems/MainActivity.java
+++ SimpleListClickableItems/app/src/main/java/com/example/meser/simplelistclickableitems/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,13 +22,23 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         paises = getResources().getStringArray(R.array.paises);
+        //para pasarle los paises a la lista necesitamos un adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 paises
         );
+        //creamos la lista
         lista = (ListView)findViewById(R.id.lista);
         lista.setAdapter(adapter);
+
+        //ponemos listener a los items de la lista
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, String.format("Has seleccionado \"%s\"",paises[position]),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
